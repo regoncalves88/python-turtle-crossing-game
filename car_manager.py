aa972a9bd4bd58a8aa825395ad1f7a09c6b155ab
Renato Gonçalves <renatogoncalves@Renatos-MacBook-Pro.local>
2021-02-01 14:38:09 -0300

    Create random car generation and implement car moving.

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,24 @@
+from turtle import Turtle
+from random import choice, randrange
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.cars = []
+
+    def create_car(self):
+        new_car = Turtle("square")
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        new_car.color(choice(COLORS))
+        new_car.penup()
+        new_car.setposition(300, randrange(-250, 250, 20))
+        new_car.setheading(180)
+        self.cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.cars:
+            car.forward(STARTING_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -9,13 +9,23 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.move_up, "Up")
 
 game_is_on = True
+loop_counter = 6
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    if loop_counter % 6 == 0:
+        car_manager.create_car()
+
+    car_manager.move_cars()
+
+    loop_counter += 1
 
 screen.exitonclick()
